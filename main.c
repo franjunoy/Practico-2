d389faf8d8685ffc88c5404ee1a68d609d002dde
franjunoy <franjunoy@hotmail.com>
2023-09-23 17:02:57 -0300

    Pilas

--- main.c
+++ main.c
@@ -8,3 +8,18 @@ void apila(pila **pilaNuevo, int a){
         pila1->sgte = a;
         *pilaNuevo=pila1;
 }
+int desapila(pila **pilaNuevo){
+    a=(*pilaNuevo)->dato;
+    pila *pilaAux;
+    if(estaVacia(pilaNuevo)==1){
+        pilaAux=*pilaNuevo;
+        *pilaNuevo=(*pilaNuevo)->sgte;
+        free(pilaAux);
+    }
+    return a;
+}
+
+
+
+
+
